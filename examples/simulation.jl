986a0a14fef00593a737965e3319a3c8d4fea366
Maya Sadeler Perrin <maya.sadeler@gmail.com>
2025-04-11 13:32:37 +0200

    example likelihood

--- examples/simulation.jl
+++ examples/simulation.jl
@@ -1,32 +1,19 @@
 using HawkesStochasticBaselineProcesses
 
-
+### simulation of the process
 
 hsb = HawkesStochasticBaseline(0.6, 1, 100; baseline = x-> min(abs(x),2))
-
-
-
 timedata = rand(hsb, 5)
 
 function baseline(x::Float64,mu::Float64)
     mu*abs(x)     
 end
 
+
+### Likelihood computation of the proceess
+
 theta = [2,0.6, 1]
+
 model = Model(timedata, baseline)
+
 likelihood(theta,model)
-
-plot(hsb, timedata,:IP)
-
-theta = [2,0.6, 1]
-### Likelihood
-function baseline(x::Float64,mu::Float64)
-    mu*abs(x)     
-end
-
-
-
-using  DataFrames
-using Integrals
-
-timedata = rand(hsb, 5)
